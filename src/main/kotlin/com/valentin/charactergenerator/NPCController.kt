b01895832bdf9a4f150c6ce9a42a2f0962386fbf
valentin-theo-sebastien-bauza <valentin.theo.sebastien.bauza@schibsted.com>
2023-02-22 15:24:59 +0100

    Controller is working and sending a NPC

--- src/main/kotlin/com/valentin/charactergenerator/NPCController.kt
+++ src/main/kotlin/com/valentin/charactergenerator/NPCController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class NPCController {
 
-    @GetMapping
+    @GetMapping("/NPC")
     fun newNPC(): NPC {
         return NPC()
     }
