d969bb64283e5d63eac669120baefcbdf6a448ed
valentin-theo-sebastien-bauza <valentin.theo.sebastien.bauza@schibsted.com>
2023-02-24 10:35:06 +0100

    Added post mapping

--- src/main/kotlin/com/valentin/charactergenerator/NPCController.kt
+++ src/main/kotlin/com/valentin/charactergenerator/NPCController.kt
@@ -1,13 +1,19 @@
 package com.valentin.charactergenerator
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class NPCController {
 
     @GetMapping("/NPC")
-    fun newNPC(): NPC {
+    fun getNPC(): NPC {
+        return NPC()
+    }
+
+    @PostMapping("/NPC")
+    fun postNPC(): NPC {
         return NPC()
     }
 }
